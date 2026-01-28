fe3b19caea041d9689e6272ab6ca861ae9f51e98
nathancarroll057 <nathancarroll057@gmail.com>
2026-01-27 18:24:52 -0600

    add shooting

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pygame
+import sys
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
 from asteroidfield import AsteroidField
 from asteroid import Asteroid
+from shot import Shot
 
 def main():
     print("Starting Asteroids with pygame version: 2.6.1")
@@ -22,6 +24,8 @@ def main():
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable,)
     field = AsteroidField()
+    shots = pygame.sprite.Group()
+    Shot.containers = (shots, updatable, drawable)
 
     while True:
         log_state()
@@ -31,11 +35,20 @@ def main():
                 return
 
         updatable.update(dt)
+
+        for asteroid in asteroids:
+            if player.collides_with(asteroid):
+                log_event("player_hit")
+                print("Game Over!")
+                sys.exit()
+
         dt = clock.tick(60) / 1000
         screen.fill("black")
 
         for obj in drawable:
             obj.draw(screen)
+
+
 
         pygame.display.flip()
 
